c9d707eed4dfdde1481553ae6857f3c64e0defe2
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 01:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
 let driverId = 0;
 let passengerId = 0;
 let tripId = 0;
-
-let store = { drivers: [], passengers: [], trips: [] };
 
 class Driver {
   constructor(name) {
