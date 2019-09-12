d008e344f29e856dfe5870a7ac454ca27e546a70
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 01:15:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,13 @@ class Passenger {
     store.passengers.push(this);
   }
   trips() {
-    return store.trips.filter(trip => {
-      return trip.passengerId == this.id;
+  return store.trips.filter(trip => {
+    return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
     });
   }
 }
